cbba508b4aea024741fdc43b7cd88f5801995689
mikilos <miki@manninger.hu>
2018-02-26 07:22:10 -0800

    Create a new Unit test.

---
+++ train-user/src/test/java/TrainUserUnutTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author meres
+ *
+ */
+public class TrainUserUnutTest {
+
+    @Test
+    public void ThisIsAnExampleTestStub() {
+        // My own test
+    }
+
+}
